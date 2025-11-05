7062ba465005291502805b74dfea2854a4c22e91
Ranjeet Yadav <rj7075yadav@gmail.com>
2025-11-06 00:19:35 +0530

    files updated

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
           playsInline
           className="w-full h-full object-cover"
         >
-          <source src={video} type="video/mp4" />
+          {/* <source src={video} type="video/mp4" /> */}
           Your browser does not support the video tag.
         </video>
       </div>
